622b7883af2a7335851890e137aaefcc10a26486
Levente Szabadkai <levente@szabadkai.com>
2026-03-29 16:11:27 +0200

    auth fix

--- src/components/AuthScreen.tsx
+++ src/components/AuthScreen.tsx
@@ -14,7 +14,7 @@ export default function AuthScreen() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: window.location.origin,
+          redirectTo: window.location.href,
         }
       });
       if (error) throw error;
