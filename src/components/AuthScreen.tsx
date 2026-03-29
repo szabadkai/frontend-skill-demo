84067d65383fd3502d934153894d334fbadac768
Levente Szabadkai <levente@szabadkai.com>
2026-03-29 16:14:44 +0200

    siteurl changes

--- src/components/AuthScreen.tsx
+++ src/components/AuthScreen.tsx
@@ -14,7 +14,7 @@ export default function AuthScreen() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: window.location.href,
+          redirectTo: `${window.location.origin}${import.meta.env.BASE_URL}`,
         }
       });
       if (error) throw error;
